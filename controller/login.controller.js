92aa35cd691194af8f58e4303e0a8ce725430b26
buff059 <kmaddt@gmail.com>
2021-05-06 13:04:00 +0700

    upload controller

---
+++ controller/login.controller.js
@@ -0,0 +1,40 @@
+const db = require('../db');
+const md5 = require('md5');
+
+module.exports.authLogin = function(req, res) {
+	res.render('auth/login');
+};
+
+
+module.exports.authLoginPOST = function(req, res) {
+	var email = req.body.email;
+	var password = req.body.password;
+
+	var user = db.get('users').find({ email: email }).value();
+
+	if(!user) {
+		res.render('auth/login', {
+			errors: [
+				'User does not exist'
+			],
+			values: req.body
+		});
+		return;
+	}
+
+
+	var hashedPass = md5(password);
+	if(user.password !== hashedPass) {
+		res.render('auth/login', {
+			errors: [
+				'Wrong password'
+			],
+			values: req.body
+		})
+		return;
+	}
+
+
+	res.cookie('userId', user.id);
+	res.redirect('/users');
+};
---
+++ controller/users.controller.js
@@ -0,0 +1,57 @@
+//--------------------------------------
+// Xu ly request va response here
+// Controller from MVC
+//--------------------------------------
+const md5 = require('md5');
+
+const db = require('../db');
+
+const shortid = require('shortid');
+
+module.exports.index = function(req, res) {
+	res.render('users/index', 
+	{
+		users: db.get('users').value()
+	});			
+}
+
+module.exports.search = function(req, res) {
+	var query = req.query.q;
+	var matched = db.get('users').value().filter(function(user) {
+		return user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+	});
+
+
+	res.render('users/index', {
+		users: matched
+	});
+}
+
+module.exports.createGET = function(req, res) {
+	res.render('users/create');
+	console.log(req.cookies);
+}
+
+
+module.exports.viewByID = function(req, res) {
+	var id = req.params.id;
+	//console.log(req.params);
+	var user = db.get('users').find({id: id}).value(); // find all keys has value id with id value received from params
+	//console.log(user);
+	if(user) { 
+		res.render('users/view', {
+			users: user
+		});
+	}else {
+		res.send('Error');
+	}
+} 
+
+module.exports.createPOST = function(req, res) {
+	// Save infomation from user's request
+	req.body.id = shortid.generate();
+	req.body.password = md5(req.body.password);
+	db.get('users').push(req.body).write();
+	// return information for user
+	res.redirect('/users');
+}
