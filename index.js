dc2699d9e305b2ed4ea45036681586cc9bbe37b9
buff059 <kmaddt@gmail.com>
2021-05-06 17:02:45 +0700

    updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = 3333;
@@ -12,7 +12,7 @@ app.set('views', './src/views');
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser('1Oasd98SJsfjdg'));
+app.use(cookieParser('process.env.SESSION_SEC'));
 // app.use(express.static('public'));
 
 app.get('/', auth.authLoginPOST, function(req, res) {
